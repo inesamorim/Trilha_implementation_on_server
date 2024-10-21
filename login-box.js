10fe046f1d4dc6871599c93f80e35e05213c46cb
inesamorim <inessamorim46@gmail.com>
2024-10-21 14:36:35 +0100

    buttons

--- login-box.js
+++ login-box.js
@@ -1,14 +1,16 @@
 window.onload = function() {
+    console.log("Script Carregado")
+
     var login_box = document.getElementById("login");
     var loginButton = document.getElementById("loginButton");
-    var closeBtn = document.getElementById("close")[0];
+    var closeBtn = document.getElementsByClassName("close")[0];
 
     loginButton.onclick = function() {
         login_box.style.display = "block";
     }
 
     closeBtn.onclick = function() {
-        login_box.style.display = none;
+        login_box.style.display = "none";
     }
 
     window.onclick = function(event) {
